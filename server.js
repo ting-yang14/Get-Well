f1df9f2c58766f5c943a6206832afeb9a00e7258
ting-yang14 <sheep870104@gmail.com>
2023-01-31 00:58:59 +0800

    feat: use https for localhost development

--- server.js
+++ server.js
@@ -1,7 +1,12 @@
 import express from "express";
 import path from "path";
+import https from "https";
+import fs from "fs";
 import { getDirname } from "./utils.js";
-
+const options = {
+  key: fs.readFileSync(`./localhost-key.pem`),
+  cert: fs.readFileSync(`./localhost.pem`),
+};
 const app = express();
 const port = 8080;
 const __dirname = getDirname(import.meta.url);
@@ -15,7 +20,8 @@ app.get("/", (req, res) => {
   res.render("index", { title: "首頁" });
 });
 app.get("/recording", (req, res) => {
-  res.send("Recording");
+  // res.send("Recording");
+  res.render("recording", { title: "復健紀錄" });
 });
 app.get("/record/:id", (req, res) => {
   res.send(`Record ID: ${req.params.id}`);
@@ -23,7 +29,10 @@ app.get("/record/:id", (req, res) => {
 app.get("/user/:id", (req, res) => {
   res.send(`User ID: ${req.params.id}`);
 });
-
-app.listen(port, function () {
+const httpsServer = https.createServer(options, app);
+httpsServer.listen(port, function () {
   console.log(`Server is running at localhost: ${port}`);
 });
+// app.listen(port, function () {
+//   console.log(`Server is running at localhost: ${port}`);
+// });
