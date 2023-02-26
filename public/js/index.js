a6592d95468f1888b4bf56539edf4277f7c1142c
ting-yang14 <sheep870104@gmail.com>
2023-02-26 14:11:26 +0800

    feat: add login and register input validation

--- public/js/index.js
+++ public/js/index.js
@@ -1,5 +1,11 @@
 import { navView } from "./navView.js";
 import { fetchUser } from "./base.js";
+import {
+  resetRegisterValidation,
+  resetLoginValidation,
+  registerInputValidation,
+  loginInputValidation,
+} from "./validation.js";
 const registerForm = document.getElementById("registerForm");
 const loginForm = document.getElementById("loginForm");
 
@@ -23,11 +29,16 @@ init();
 
 loginBtn.addEventListener("click", login);
 registerBtn.addEventListener("click", register);
-toggleLoginBtn.addEventListener("click", resetResponseMsg.bind(null, loginMsg));
-toggleRegisterBtn.addEventListener(
-  "click",
-  resetResponseMsg.bind(null, registerMsg)
-);
+toggleLoginBtn.addEventListener("click", () => {
+  resetInputValue();
+  resetResponseMsg(loginMsg);
+  resetLoginValidation();
+});
+toggleRegisterBtn.addEventListener("click", () => {
+  resetInputValue();
+  resetResponseMsg(registerMsg);
+  resetRegisterValidation();
+});
 
 async function init() {
   if (localStorage.token) {
@@ -46,53 +57,73 @@ async function init() {
 }
 
 async function login() {
-  const requestBody = {
-    email: loginEmail.value,
-    password: loginPassword.value,
-  };
-  try {
-    const response = await axios.post("/api/user/login", requestBody);
-    console.log(response.data);
-    displayResponseMsg(loginMsg, "成功登入", "text-success");
-    loginForm.reset();
-    localStorage.setItem("token", response.data.data.token);
-    window.location.href = "/user";
-  } catch (error) {
-    console.error(error);
-    console.log(error.response.data.message);
-    displayResponseMsg(loginMsg, error.response.data.message, "text-danger");
+  if (loginInputValidation()) {
+    const requestBody = {
+      email: loginEmail.value,
+      password: loginPassword.value,
+    };
+    try {
+      const response = await axios.post("/api/user/login", requestBody);
+      console.log(response.data);
+      displayResponseMsg(loginMsg, "成功登入", "text-success");
+      loginForm.reset();
+      localStorage.setItem("token", response.data.data.token);
+      window.location.href = "/user";
+    } catch (error) {
+      console.error(error);
+      console.log(error.response.data.message);
+      displayResponseMsg(loginMsg, error.response.data.message, "text-danger");
+    }
+  } else {
+    return;
   }
 }
 
 async function register() {
-  const requestBody = {
-    username: registerUsername.value,
-    email: registerEmail.value,
-    password: registerPassword.value,
-  };
-  try {
-    const response = await axios.post("/api/user/register", requestBody);
-    console.log(response.data);
-    displayResponseMsg(registerMsg, "成功註冊", "text-success");
-    registerForm.reset();
-    toggleLoginBtn.classList.remove("btn-secondary");
-    toggleLoginBtn.classList.add("btn-success");
-    loginEmail.value = requestBody.email;
-    resetResponseMsg(loginMsg);
-  } catch (error) {
-    console.error(error);
-    console.log(error.response.data.message);
-    displayResponseMsg(registerMsg, error.response.data.message, "text-danger");
+  if (registerInputValidation()) {
+    const requestBody = {
+      username: registerUsername.value,
+      email: registerEmail.value,
+      password: registerPassword.value,
+    };
+    try {
+      const response = await axios.post("/api/user/register", requestBody);
+      console.log(response.data);
+      displayResponseMsg(registerMsg, "成功註冊", "text-success");
+      registerForm.reset();
+      toggleLoginBtn.classList.remove("btn-secondary");
+      toggleLoginBtn.classList.add("btn-success");
+      loginEmail.value = requestBody.email;
+      resetRegisterValidation();
+      resetResponseMsg(loginMsg);
+    } catch (error) {
+      console.error(error);
+      console.log(error.response.data.message);
+      displayResponseMsg(
+        registerMsg,
+        error.response.data.message,
+        "text-danger"
+      );
+    }
+  } else {
+    return;
   }
 }
 
 function displayResponseMsg(item, msg, className) {
   resetResponseMsg(item);
-  item.innerText = msg;
+  item.textContent = msg;
   item.classList.add(className);
 }
 
 function resetResponseMsg(item) {
-  item.innerText = null;
+  item.textContent = null;
   item.classList.remove("text-danger", "text-success");
+}
+function resetInputValue() {
+  registerUsername.value = null;
+  registerEmail.value = null;
+  registerPassword.value = null;
+  loginEmail.value = null;
+  loginPassword.value = null;
 }
--- public/js/validation.js
+++ public/js/validation.js
@@ -1,3 +1,4 @@
+// record and recording page
 const exerciseNameValidation = document.getElementById(
   "exerciseNameValidation"
 );
@@ -54,6 +55,151 @@ export function exerciseInputValidation() {
   }
 
   if (isExerciseNameValid && isExerciseCountsValid) {
+    return true;
+  } else {
+    return false;
+  }
+}
+
+// register and login
+const registerUsernameValidation = document.getElementById(
+  "registerUsernameValidation"
+);
+const registerEmailValidation = document.getElementById(
+  "registerEmailValidation"
+);
+const registerPasswordValidation = document.getElementById(
+  "registerPasswordValidation"
+);
+const loginEmailValidation = document.getElementById("loginEmailValidation");
+const loginPasswordValidation = document.getElementById(
+  "loginPasswordValidation"
+);
+const usernameRegex = /^[\u4E00-\u9FFFa-zA-Z0-9]{1,8}$/;
+const emailRegex = /^([a-zA-Z0-9._%+-]+)@([a-zA-Z0-9.-]+)\.([a-zA-Z]{2,})$/;
+const passwordRegex =
+  /^(?=.*[a-zA-Z])(?=.*\d)(?=.*[@$!%*?&])[A-Za-z\d@$!%*?&]{6,}$/;
+
+export function resetRegisterValidation() {
+  // reset
+  registerUsernameValidation.classList = [];
+  registerEmailValidation.classList = [];
+  registerPasswordValidation.classList = [];
+  registerUsernameValidation.textContent = "";
+  registerEmailValidation.textContent = "";
+  registerPasswordValidation.textContent = "";
+  inputClassName.forEach((className) => {
+    if (registerUsername.classList.contains(className)) {
+      registerUsername.classList.remove(className);
+    }
+    if (registerEmail.classList.contains(className)) {
+      registerEmail.classList.remove(className);
+    }
+    if (registerPassword.classList.contains(className)) {
+      registerPassword.classList.remove(className);
+    }
+  });
+}
+
+export function resetLoginValidation() {
+  // reset
+  loginEmailValidation.classList = [];
+  loginPasswordValidation.classList = [];
+  loginEmailValidation.textContent = "";
+  loginPasswordValidation.textContent = "";
+  inputClassName.forEach((className) => {
+    if (loginEmail.classList.contains(className)) {
+      loginEmail.classList.remove(className);
+    }
+    if (loginPassword.classList.contains(className)) {
+      loginPassword.classList.remove(className);
+    }
+  });
+}
+
+export function registerInputValidation() {
+  resetRegisterValidation();
+  //validate
+  const registerUsernameValue = registerUsername.value;
+  const registerEmailValue = registerEmail.value;
+  const registerPasswordValue = registerPassword.value;
+  const isRegisterUsernameValid = usernameRegex.test(registerUsernameValue);
+  const isRegisterEmailValid = emailRegex.test(registerEmailValue);
+  const isRegisterPasswordValid = passwordRegex.test(registerPasswordValue);
+  if (isRegisterUsernameValid) {
+    registerUsername.classList.add(inputClassName[0]);
+    registerUsernameValidation.classList.add(tooltipClassName[0]);
+    registerUsernameValidation.textContent = "格式正確";
+  } else {
+    registerUsername.classList.add(inputClassName[1]);
+    registerUsernameValidation.classList.add(tooltipClassName[1]);
+    registerUsernameValidation.textContent =
+      "請輸入一至八字元的數字、中英文字母組合";
+  }
+
+  if (isRegisterEmailValid) {
+    registerEmail.classList.add(inputClassName[0]);
+    registerEmailValidation.classList.add(tooltipClassName[0]);
+    registerEmailValidation.textContent = "格式正確";
+  } else {
+    registerEmail.classList.add(inputClassName[1]);
+    registerEmailValidation.classList.add(tooltipClassName[1]);
+    registerEmailValidation.textContent = "請輸入正確信箱格式";
+  }
+
+  if (isRegisterPasswordValid) {
+    registerPassword.classList.add(inputClassName[0]);
+    registerPasswordValidation.classList.add(tooltipClassName[0]);
+    registerPasswordValidation.textContent = "格式正確";
+  } else {
+    registerPassword.classList.add(inputClassName[1]);
+    registerPasswordValidation.classList.add(tooltipClassName[1]);
+    registerPasswordValidation.textContent =
+      "請輸入含有數字、英文字母和標點符號（如！和＆）的密碼組合，且至少由六個字元組成";
+  }
+
+  if (
+    isRegisterUsernameValid &&
+    isRegisterEmailValid &&
+    isRegisterPasswordValid
+  ) {
+    return true;
+  } else {
+    return false;
+  }
+}
+
+export function loginInputValidation() {
+  resetLoginValidation();
+  //validate
+  const loginEmailValue = loginEmail.value;
+  const loginPasswordValue = loginPassword.value;
+
+  const isLoginEmailValid = emailRegex.test(loginEmailValue);
+  const isLoginPasswordValid = passwordRegex.test(loginPasswordValue);
+
+  if (isLoginEmailValid) {
+    loginEmail.classList.add(inputClassName[0]);
+    loginEmailValidation.classList.add(tooltipClassName[0]);
+    loginEmailValidation.textContent = "格式正確";
+  } else {
+    loginEmail.classList.add(inputClassName[1]);
+    loginEmailValidation.classList.add(tooltipClassName[1]);
+    loginEmailValidation.textContent = "請輸入正確信箱格式";
+  }
+
+  if (isLoginPasswordValid) {
+    loginPassword.classList.add(inputClassName[0]);
+    loginPasswordValidation.classList.add(tooltipClassName[0]);
+    loginPasswordValidation.textContent = "格式正確";
+  } else {
+    loginPassword.classList.add(inputClassName[1]);
+    loginPasswordValidation.classList.add(tooltipClassName[1]);
+    loginPasswordValidation.textContent =
+      "請輸入含有數字、英文字母和標點符號（如！和＆）的密碼組合，且至少由六個字元組成";
+  }
+
+  if (isLoginEmailValid && isLoginPasswordValid) {
     return true;
   } else {
     return false;
